82369b7bf4c7ff994e795868616c4247b9200c98
TheAnnoying <drork2018@gmail.com>
2023-09-23 14:11:36 +0300

    🔗 removed font url

--- src/components/FontCategorySelect.svelte
+++ src/components/FontCategorySelect.svelte
@@ -6,22 +6,22 @@
         {
             id: "Minecraft",
             fontList: [
-                [ "Five", "/fonts/minecraft/five.ttf", "13px", "Minecraft" ],
-                [ "Five Bold", "/fonts/minecraft/fivebold.ttf", "13px", "Minecraft" ],
-                [ "Seven", "/fonts/minecraft/seven.ttf", "13px", "Minecraft" ],
+                [ "Five", "13px", "Minecraft" ],
+                [ "Five Bold", "13px", "Minecraft" ],
+                [ "Seven", "13px", "Minecraft" ],
             ]
         },
         { 
             id: "FloideN",
             fontList: [
-                [ "MC Eleven", "https://fontstruct.com/font_archives/download/2308706/", "13px", "FloideN" ],
-                [ "MC Nine", "https://fontstruct.com/font_archives/download/2310239/", "13px", "FloideN" ],
-                [ "MC Ten Alt", "https://fontstruct.com/font_archives/download/2306021/", "13px", "FloideN" ],
-                [ "MC Ten Fire", "https://fontstruct.com/font_archives/download/2299443/", "13px", "FloideN" ],
-                [ "MC Terra", "https://fontstruct.com/font_archives/download/2308712/", "13px", "FloideN" ],
-                [ "Minecraft Bold Ten", "https://fontstruct.com/font_archives/download/2287385/", "13px", "FloideN" ],
-                [ "Minecraft Thick Ten", "https://fontstruct.com/font_archives/download/2296934/", "13px", "FloideN" ],
-                [ "Modified MC Ten Drip", "https://fontstruct.com/font_archives/download/2275860/", "13px", "FloideN" ]
+                [ "MC Eleven", "13px", "FloideN" ],
+                [ "MC Nine", "13px", "FloideN" ],
+                [ "MC Ten Alt", "13px", "FloideN" ],
+                [ "MC Ten Fire", "13px", "FloideN" ],
+                [ "MC Terra", "13px", "FloideN" ],
+                [ "Minecraft Bold Ten", "12px", "FloideN" ],
+                [ "Minecraft Thick Ten", "13px", "FloideN" ],
+                [ "Modified MC Ten Drip", "13px", "FloideN" ]
             ]
         }
     ];
