6c2e7b6cb3ceb14d1a1e3f2648239c1550dd3db2
TheAnnoying <drork2018@gmail.com>
2023-09-23 22:25:37 +0300

    🍫 made canvas and the input value part of store

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -3,4 +3,6 @@ import { writable } from "svelte/store";
 export let
     currentWidth = writable(0),
     currentHeight = writable(0),
-    currentFont = writable({ id: "patred/minecraft-ten-lowercase.ttf", size: "13px" });
+    currentFont = writable({ id: "patred/minecraft-ten-lowercase.ttf", size: "13px" }),
+    canvas = writable(null),
+    value = writable(null);
