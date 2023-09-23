40b07f3713c00d8a4811a710c08cc33ef83ab53c
TheAnnoying <drork2018@gmail.com>
2023-09-23 14:12:15 +0300

    📃 updated all array indexes

--- src/components/FontDialog.svelte
+++ src/components/FontDialog.svelte
@@ -4,17 +4,16 @@
 
     let dialog;
     const clean = (input) => input.toLowerCase().replace(/ +/g, "-");
-    //somehow update the calculation
 </script>
 <button on:click={() => dialog.showModal()}>{category.id}</button>
 <dialog bind:this={dialog}>
     {#each category.fontList as font}
-        <button style="font-family: '{clean(font[3])}/{clean(font[0])}'" on:click={() => $currentFont = { id: `${clean(font[3])}/${clean(font[0])}`, size: font[2] }}>{font[0]}</button>
+        <button style="font-family: '{clean(font[2])}/{clean(font[0])}'" on:click={() => $currentFont = { id: `${clean(font[2])}/${clean(font[0])}`, size: font[1] }}>{font[0]}</button>
         {@html `
         <style>
             @font-face {
-                font-family: "${clean(font[3])}/${clean(font[0])}";
-                src: url("/fonts/${clean(font[3])}/${clean(font[0])}.ttf");
+                font-family: "${clean(font[2])}/${clean(font[0])}";
+                src: url("/fonts/${clean(font[2])}/${clean(font[0])}.ttf");
             }
         </style>`
         }
