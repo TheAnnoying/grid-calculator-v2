5c5e70b90eaa1fb0cf13faacd59208eefb9ad96e
TheAnnoying <drork2018@gmail.com>
2023-09-30 21:39:30 +0300

    🙈 made result no longer show current font

--- src/components/Result.svelte
+++ src/components/Result.svelte
@@ -1,6 +1,5 @@
 <script>
-    import { currentWidth, currentHeight, currentFont } from "../lib/store";
+    import { currentWidth, currentHeight } from "../lib/store";
 </script>
 <p>width: {$currentWidth - 1}</p>
 <p>height: {$currentHeight -1}</p>
-<p>{$currentFont.id}</p>
