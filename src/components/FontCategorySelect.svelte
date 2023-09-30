2b2a5d9d6b099ffbb4031a725f00e587c91a75d0
TheAnnoying <drork2018@gmail.com>
2023-09-30 21:37:39 +0300

    🚿 made select button show the current font's name

--- src/components/FontCategorySelect.svelte
+++ src/components/FontCategorySelect.svelte
@@ -1,12 +1,13 @@
 <script>
     import FontDialog from "./FontDialog.svelte";
+    import { currentFont } from "../lib/store";
     let dialog;
 
     const fonts = [
         {
             id: "Minecraft",
             fontList: [
-                [ "Five", "13px", "Minecraft" ],
+                [ "Five", "7px", "Minecraft" ],
                 [ "Five Bold", "13px", "Minecraft" ],
                 [ "Seven", "13px", "Minecraft" ],
             ]
@@ -23,10 +24,31 @@
                 [ "Minecraft Thick Ten", "13px", "FloideN" ],
                 [ "Modified MC Ten Drip", "13px", "FloideN" ]
             ]
+        },
+        {
+            id: "7pxls",
+            fontList: [
+                [ "Eight", "13px", "7pxls" ],
+                [ "Hideaway GUI", "13px", "7pxls" ],
+                [ "MC Ten Elongated Fix", "13px", "7pxls" ],
+                [ "MC Ten Elongated Middle", "13px", "7pxls" ],
+                [ "MC Ten Elongated Reverse", "13px", "7pxls" ],
+                [ "Minecraft Ten Round", "13px", "7pxls" ],
+                [ "Mojang Pixel", "13px", "7pxls" ],
+                [ "Nine", "13px", "7pxls" ],
+                [ "Seven Round", "13px", "7pxls" ],
+                [ "Seven", "13px", "7pxls" ]
+            ]
+        },
+        {
+            id: "Patred",
+            fontList: [
+                [ "Minecraft Ten Lowercase", "13px", "Patred" ]
+            ]
         }
     ];
 </script>
-<button on:click={() => dialog.showModal()}>What font are you using?</button>
+<button on:click={() => dialog.showModal()}>{$currentFont.name ?? "Select font"}</button>
 <dialog id="modal" bind:this={dialog}>
     <h1>Select category</h1>
     {#each fonts as category}
