5baf114fc023d8ad3374ca44dcf7c1d0988d7461
TheAnnoying <drork2018@gmail.com>
2023-10-01 22:16:57 +0300

    💀 updated the entire calculation system

--- src/lib/updateDimensions.js
+++ src/lib/updateDimensions.js
@@ -2,17 +2,40 @@ import { currentWidth, currentHeight, currentFont, canvas, value } from "./store
 import { get } from "svelte/store";
 
 export function updateDimensions() {
-    if([0, null].includes(get(value).length)) {
+    if(get(value).trim() === "") {
         currentWidth.set(1);
         currentHeight.set(1);
         return;
     }
 
-    const context = get(canvas).getContext("2d");
-    context.font = `${get(currentFont).size} '${get(currentFont).id}'`;
+    const canvasElement = get(canvas);
+    const context = canvasElement.getContext("2d");
 
-    const measure = context.measureText(get(value));
+    context.fillStyle = "black";
+    context.fillRect(0, 0, canvasElement.width, canvasElement.height);
 
-    currentHeight.set(Math.round(measure.actualBoundingBoxAscent));
-    currentWidth.set(Math.round(measure.width));
+    const multiplyFactor = 8;
+    context.font = `${get(currentFont).size*multiplyFactor}px '${get(currentFont).id}'`;
+    context.textBaseline = "top";
+
+    context.fillStyle = "red";
+    context.fillText(get(value), 0, 0);
+    
+    const imgData = context.getImageData(0, 0, canvasElement.width, canvasElement.height).data;
+
+    let minX = Infinity, minY = Infinity;
+    let maxX = -Infinity, maxY = -Infinity;
+    for (let y = 0; y < canvasElement.height; y++) for (let x = 0; x < canvasElement.width; x++) {
+        const index = y*(canvasElement.width*4)+x*4;
+        const pixel = imgData.slice(index, index + 4);
+        if (pixel[0] === 255) {
+            minX = Math.min(minX, x);
+            maxX = Math.max(maxX, x);
+            minY = Math.min(minY, y);
+            maxY = Math.max(maxY, y);
+        }
+    }
+    
+    currentHeight.set(Math.round((maxY-minY)*(1/multiplyFactor)));
+    currentWidth.set(Math.round((maxX-minX)*(1/multiplyFactor)));
 }
