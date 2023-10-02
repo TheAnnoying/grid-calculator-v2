e72c34e32cbc83c76cba44f0794a9d50999842a2
TheAnnoying <drork2018@gmail.com>
2023-10-02 10:47:54 +0300

    🐛 aligned text in the center

--- src/lib/updateDimensions.js
+++ src/lib/updateDimensions.js
@@ -13,13 +13,14 @@ export function updateDimensions() {
 
     context.fillStyle = "black";
     context.fillRect(0, 0, canvasElement.width, canvasElement.height);
-    
+
     const multiplyFactor = 8;
     context.font = `${get(currentFont).size*multiplyFactor}px '${get(currentFont).id}'`;
     context.textBaseline = "middle";
+    context.textAlign = "center";
 
     context.fillStyle = "red";
-    context.fillText(get(value), 0, canvasElement.height/2);
+    context.fillText(get(value), canvasElement.width/2, canvasElement.height/2);
     
     const imgData = context.getImageData(0, 0, canvasElement.width, canvasElement.height).data;
 
@@ -28,14 +29,14 @@ export function updateDimensions() {
     for (let y = 0; y < canvasElement.height; y++) for (let x = 0; x < canvasElement.width; x++) {
         const index = y*(canvasElement.width*4)+x*4;
         const pixel = imgData.slice(index, index + 4);
-        if (pixel[0] === 255) {
+        if (pixel[0] !== 0) {
             minX = Math.min(minX, x);
             maxX = Math.max(maxX, x);
             minY = Math.min(minY, y);
             maxY = Math.max(maxY, y);
         }
     }
-    
-    currentHeight.set(Math.round((maxY-minY)*(1/multiplyFactor)));
-    currentWidth.set(Math.round((maxX-minX)*(1/multiplyFactor)));
+
+    currentHeight.set(Math.round((maxY-minY)/multiplyFactor));
+    currentWidth.set(Math.round((maxX-minX)/multiplyFactor));
 }
