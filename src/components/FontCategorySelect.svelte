99fc02be92ccb2ea10f76e29c381e68a1c8ca119
TheAnnoying <drork2018@gmail.com>
2023-10-07 17:12:52 +0300

    🥳 added new fonts

--- src/components/FontCategorySelect.svelte
+++ src/components/FontCategorySelect.svelte
@@ -14,35 +14,36 @@
 		{
 			id: "FloideN",
 			fontList: [
-				[ "MC Eleven", 13, "FloideN" ],
-				[ "MC Nine", 13, "FloideN" ],
-				[ "MC Ten Alt", 13, "FloideN" ],
-				[ "MC Ten Fire", 13, "FloideN" ],
-				[ "MC Terra", 13, "FloideN" ],
+				[ "MC Eleven", 14, "FloideN" ],
+				[ "MC Nine", 9, "FloideN" ],
+				[ "MC Ten Alt", 9, "FloideN" ],
+				[ "MC Ten Fire", 16, "FloideN" ],
+				[ "MC Terra", 11, "FloideN" ],
 				[ "Minecraft Bold Ten", 12, "FloideN" ],
-				[ "Minecraft Thick Ten", 13, "FloideN" ],
-				[ "Modified MC Ten Drip", 13, "FloideN" ]
+				[ "Minecraft Thick Ten", 8, "FloideN" ],
+				[ "Modified MC Ten Drip", 14, "FloideN" ]
 			]
 		},
 		{
 			id: "7pxls",
 			fontList: [
-				[ "Eight", 13, "7pxls" ],
-				[ "Hideaway GUI", 13, "7pxls" ],
-				[ "MC Ten Elongated Fix", 13, "7pxls" ],
-				[ "MC Ten Elongated Middle", 13, "7pxls" ],
-				[ "MC Ten Elongated Reverse", 13, "7pxls" ],
-				[ "Minecraft Ten Round", 13, "7pxls" ],
-				[ "Mojang Pixel", 13, "7pxls" ],
-				[ "Nine", 13, "7pxls" ],
-				[ "Seven Round", 13, "7pxls" ],
-				[ "Seven", 13, "7pxls" ]
+				[ "Eight", 8, "7pxls" ],
+				[ "Hideaway GUI", 8, "7pxls" ],
+				[ "MC Ten Elongated Fix", 14, "7pxls" ],
+				[ "Mojang Pixel", 8, "7pxls" ],
+				[ "Nine", 9, "7pxls" ],
+				[ "Seven Round", 7, "7pxls" ],
+				[ "Seven", 7, "7pxls" ]
 			]
 		},
 		{
 			id: "Patred",
 			fontList: [
-				[ "Minecraft Ten Lowercase", 13, "Patred" ]
+				[ "Minecraft Ten Lowercase", 13, "Patred" ],
+				[ "Dungeons v2 AltLw", 13, "Patred" ],
+				[ "MC Ten Drip", 13, "Patred" ],
+				[ "MC Ten Round", 13, "Patred" ],
+				[ "MC Ten Round Drip", 15, "Patred" ]
 			]
 		}
 	];
