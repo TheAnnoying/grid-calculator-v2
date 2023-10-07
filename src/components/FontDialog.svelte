80c70b1d50a4b23b6ecdc62fd85d640f68c7385c
TheAnnoying <drork2018@gmail.com>
2023-10-07 11:12:56 +0300

    📜 fixed scrolling

--- src/components/FontDialog.svelte
+++ src/components/FontDialog.svelte
@@ -15,27 +15,34 @@
 <button class="button" on:click={() => dialog.showModal()}>{category.id}</button>
 <dialog bind:this={dialog}>
 	<button class="dialog-close" on:click={() => dialog.close()}><img src="/x.svg" alt="X Icon" /></button>
-	<div id="font-grid">
-		{#each category.fontList as font}
-			<div class="data">
-				<p>{font[0]}</p>
-				<span style="font-family: '{clean(font[2])}/{clean(font[0])}'; text-transform: uppercase;">The quick brown fox jumps over the lazy dog</span>
-				<button class="button button-gray" on:click={() => selectFont(font)}>USE</button>
-			</div>
-			{@html `
-				<style>
-					@font-face {
-						font-family: "${clean(font[2])}/${clean(font[0])}";
-						src: url("/fonts/${clean(font[2])}/${clean(font[0])}.ttf");
-					}
-				</style>
-			`}
-		{/each}
+	<div id="scroll">
+		<div id="font-grid">
+			{#each category.fontList as font}
+				<div class="data">
+					<p>{font[0]}</p>
+					<span style="font-family: '{clean(font[2])}/{clean(font[0])}'; text-transform: uppercase;">The quick brown fox jumps over the lazy dog</span>
+					<button class="button button-gray" on:click={() => selectFont(font)}>USE</button>
+				</div>
+				{@html `
+					<style>
+						@font-face {
+							font-family: "${clean(font[2])}/${clean(font[0])}";
+							src: url("/fonts/${clean(font[2])}/${clean(font[0])}.ttf");
+						}
+					</style>
+				`}
+			{/each}
+		</div>
 	</div>
 </dialog>
 <style>
 	dialog {
 		max-height: 80vh;
+	}
+
+	#scroll {
+		max-height: 80vh;
+		overflow: auto;
 	}
 
 	#font-grid {
